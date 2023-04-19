41ddc74e6611a1b5931acacd8d6e5cbe6ddb01b7
juuulek <julian.nawalka@gmail.com>
2023-04-19 17:16:48 +0200

    Initial commit

---
+++ src/main/java/io/example/advancetodo/FinalCoderslabProjectApplication.java
@@ -0,0 +1,13 @@
+package io.example.advancetodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCoderslabProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalCoderslabProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/example/advancetodo/FinalCoderslabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.example.advancetodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalCoderslabProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

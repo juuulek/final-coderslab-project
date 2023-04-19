f3fb6556d2392e02e16fd647c7438407edcb9106
juuulek <julian.nawalka@gmail.com>
2023-04-19 19:55:38 +0200

    Encja użytkownika

---
+++ src/main/java/io/example/advancetodo/domain/User.java
@@ -0,0 +1,27 @@
+package io.example.advancetodo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    // logowaniem i hasłami użytkowników dziś się nie zajmuję
+    // na ten moment "logowanie" będzie odbywało się bez żadnej autoryzacji;
+    // Spring Security jest przygotowany w pomie, ale nie zakładam nauczenia się go przed obroną
+}

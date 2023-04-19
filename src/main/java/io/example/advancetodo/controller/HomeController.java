f49d3a0df0fb67262f6283b906b1043f440de00d
juuulek <julian.nawalka@gmail.com>
2023-04-19 18:19:46 +0200

    Placeholder strony startowej

---
+++ src/main/java/io/example/advancetodo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package io.example.advancetodo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        Random random = new Random();
+        int x = random.nextInt();
+        long y = random.nextInt();
+        return "Witaj!<br \\>" +
+                "Tu niestety jeszcze nic nie ma :(<br \\>" +
+                "A wiedziałaś/łeś, że " + x + " × " + y + " = " + (x * y);
+    }
+}

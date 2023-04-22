6de59992c86ea2e00c3739224be268c3c76832fc
juuulek <julian.nawalka@gmail.com>
2023-04-22 20:24:17 +0200

    Encja zadań — tymczasowo skończona.

--- src/main/java/io/example/advancetodo/domain/Task.java
+++ src/main/java/io/example/advancetodo/domain/Task.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,5 +21,24 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @NotNull
+    private TaskList list;
 
+    @Column(columnDefinition = "varchar(63)", nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    private LocalDateTime apperance;
+
+    private LocalDateTime alert;
+
+    private LocalDateTime deadline;
+
+    private LocalDateTime done;
+
+    @Column(columnDefinition = "text")
+    private String tags;
 }

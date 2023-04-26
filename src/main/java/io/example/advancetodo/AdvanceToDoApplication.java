98c8c59f8c05ddc860dbb50b82bc731a145fe8f3
juuulek <julian.nawalka@gmail.com>
2023-04-26 23:38:26 +0200

    Podmiana wykomentowania Spring Security na wykluczenie

--- src/main/java/io/example/advancetodo/AdvanceToDoApplication.java
+++ src/main/java/io/example/advancetodo/AdvanceToDoApplication.java
@@ -2,10 +2,11 @@ package io.example.advancetodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class AdvanceToDoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AdvanceToDoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdvanceToDoApplication.class, args);
+    }
 }

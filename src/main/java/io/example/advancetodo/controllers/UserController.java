009ba8e9cfa5916acbb1693f2abc0fa7ca47313c
juuulek <julian.nawalka@gmail.com>
2023-05-01 02:43:37 +0200

    Pierwsze serwis i kontroler

--- src/main/java/io/example/advancetodo/controllers/UserController.java
+++ src/main/java/io/example/advancetodo/controllers/UserController.java
@@ -1,15 +1,41 @@
 package io.example.advancetodo.controllers;
 
+import io.example.advancetodo.dtos.UserDto;
 import io.example.advancetodo.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/api/users")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @Operation (summary = "Gets all users", description = "Gets list of all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful response",
+                    content = @Content(schema = @Schema(implementation = UserDto[].class))),
+            @ApiResponse(responseCode = "404", description = "Users cannot be found")
+    })
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAll () {
+        List <UserDto> users = userService.getAll();
+        if (users.isEmpty())
+            return ResponseEntity.notFound().build();
+        else
+            return ResponseEntity.ok(users);
+    }
+
 
     // to do
 }
--- src/main/java/io/example/advancetodo/services/UserService.java
+++ src/main/java/io/example/advancetodo/services/UserService.java
@@ -1,15 +1,23 @@
 package io.example.advancetodo.services;
 
+import io.example.advancetodo.dtos.UserDto;
 import io.example.advancetodo.mappers.UserMapper;
 import io.example.advancetodo.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+
+    public List<UserDto> getAll() {
+        return userMapper.mapToDto(userRepository.findAll()); // nie podoba mi się tu "find"
+    }
+
 
     // to do
 }

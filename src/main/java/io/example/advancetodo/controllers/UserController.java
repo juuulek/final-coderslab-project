aef7c52bdc011dc9b47b9bbdc1d83053dab81a3a
juuulek <julian.nawalka@gmail.com>
2023-05-01 21:49:06 +0200

    Dodanie do Usera POST

--- src/main/java/io/example/advancetodo/controllers/UserController.java
+++ src/main/java/io/example/advancetodo/controllers/UserController.java
@@ -9,10 +9,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,19 +20,27 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-    @Operation (summary = "Gets all users", description = "Gets list of all users")
+    @Operation(summary = "Gets all users", description = "Gets list of all users")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful response",
                     content = @Content(schema = @Schema(implementation = UserDto[].class))),
             @ApiResponse(responseCode = "404", description = "Users cannot be found")
     })
     @GetMapping
-    public ResponseEntity<List<UserDto>> getAll () {
-        List <UserDto> users = userService.getAll();
+    public ResponseEntity<List<UserDto>> getAll() {
+        List<UserDto> users = userService.getAll();
         if (users.isEmpty())
             return ResponseEntity.notFound().build();
         else
             return ResponseEntity.ok(users);
+    }
+
+    // pozostałe getty (by id, by login oraz by mail
+
+    @PostMapping
+    public ResponseEntity<UserDto> add(@RequestBody @Valid UserDto dto) {
+        dto = userService.add(dto);
+        return ResponseEntity.ok(dto);
     }
 
 
--- src/main/java/io/example/advancetodo/services/UserService.java
+++ src/main/java/io/example/advancetodo/services/UserService.java
@@ -1,10 +1,12 @@
 package io.example.advancetodo.services;
 
 import io.example.advancetodo.dtos.UserDto;
+import io.example.advancetodo.entities.User;
 import io.example.advancetodo.mappers.UserMapper;
 import io.example.advancetodo.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -16,6 +18,15 @@ public class UserService {
 
     public List<UserDto> getAll() {
         return userMapper.mapToDto(userRepository.findAll()); // nie podoba mi się tu "find"
+    }
+
+    // pozostałe getty (by id, by login oraz by mail
+
+    public UserDto add(UserDto dto) {
+        User user = userMapper.mapToEntity(dto);
+        Assert.isNull(user.getId(), "Id has to be null");
+        userRepository.save(user);
+        return userMapper.mapToDto(user);
     }
 
 

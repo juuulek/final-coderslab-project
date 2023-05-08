bd1703f1f1f9434be339c68a4c2e659131e17c21
juuulek <julian.nawalka@gmail.com>
2023-05-08 21:25:03 +0200

    Podmiana setterów w Task tak, aby nie można było zrobić taska ze złymi chronologicznie datami (np. deadline przed alertem)

--- src/main/java/io/example/advancetodo/entities/Task.java
+++ src/main/java/io/example/advancetodo/entities/Task.java
@@ -36,4 +36,52 @@ public class Task {
 
     @Column(columnDefinition = "text")
     private String tags;
+
+    public void setAppearance(LocalDateTime appearance) {
+        if (appearance != null) {
+            if (alert != null && alert.isBefore(appearance))
+                throw new IllegalArgumentException("Appearance cannot be after alert");
+            if (deadline != null && deadline.isBefore(appearance))
+                throw new IllegalArgumentException("Appearance cannot be after deadline");
+            if (done != null && done.isBefore(appearance))
+                throw new IllegalArgumentException("Appearance cannot be after done");
+        }
+        this.appearance = appearance;
+    }
+
+    public void setAlert(LocalDateTime alert) {
+        if (alert != null) {
+            if (appearance != null && appearance.isAfter(alert))
+                throw new IllegalArgumentException("Alert cannot be before appearance");
+            if (deadline != null && deadline.isBefore(alert))
+                throw new IllegalArgumentException("Alert cannot be after deadline");
+            if (done != null && done.isBefore(alert))
+                throw new IllegalArgumentException("Alert cannot be after done");
+        }
+        this.alert = alert;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        if (deadline != null) {
+            if (appearance != null && appearance.isAfter(deadline))
+                throw new IllegalArgumentException("Deadline cannot be before appearance");
+            if (alert != null && alert.isAfter(deadline))
+                throw new IllegalArgumentException("Deadline cannot be before alert");
+            if (done != null && done.isBefore(deadline))
+                throw new IllegalArgumentException("Deadline cannot be after done");
+        }
+        this.deadline = deadline;
+    }
+
+    public void setDone(LocalDateTime done) {
+        if (done != null) {
+            if (appearance != null && appearance.isAfter(done))
+                throw new IllegalArgumentException("Done cannot be before appearance");
+            if (alert != null && alert.isAfter(done))
+                throw new IllegalArgumentException("Done cannot be before alert");
+            if (deadline != null && deadline.isAfter(done))
+                throw new IllegalArgumentException("Done cannot be before deadline");
+        }
+        this.done = done;
+    }
 }
